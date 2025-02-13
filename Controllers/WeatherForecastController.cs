1da2875194e82ef8a353f1c330afd7301b502fe9
vasrsha <varsha.iihglobal@gmail.com>
2025-02-13 14:58:26 +0530

    add log

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace GitTestDemo.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("test");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
